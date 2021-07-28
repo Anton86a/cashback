021570e9225885eee90951b459ae2ccf8fe373b7
Anton <chebotnikov-av@mail.ru>
2021-07-28 19:02:44 +0500

    refactoring

--- js/app.js
+++ js/app.js
@@ -18,20 +18,11 @@ console.log(cashback);
 
 function handleSubmit(evt) {
     evt.preventDefault();
-
-    const specialAmountErrorEl = document.getElementById('special-amount-error');
-    const otherAmountErrorEl = document.getElementById('other-amount-error');
-    const specialCashbackEl = document.getElementById('special-cashback');
-    const otherCashbackEl = document.getElementById('other-cashback');
-    const totalCashbackEl = document.getElementById('total-cashback');
-
     specialAmountErrorEl.textContent = '';
     otherAmountErrorEl.textContent = '';
     specialCashbackEl.textContent = '';
     otherCashbackEl.textContent = '';
     totalCashbackEl.textContent = '';
-
-    const specialAmountInputEl = document.getElementById('special-amount-input');
     const specialAmount = Number(specialAmountInputEl.value);
     if (Number.isNaN(specialAmount)) {
         specialAmountErrorEl.textContent = 'Неверное значение. Введите число, например: 10000';
@@ -41,7 +32,6 @@ function handleSubmit(evt) {
         specialAmountErrorEl.textContent = 'Слишком большое значение. Введите число, например: 10000';
         return;
     }
-    const otherAmountInputEl = document.getElementById('other-amount-input');
     const otherAmount = Number(otherAmountInputEl.value);
     if (Number.isNaN(otherAmount)) {
         otherAmountErrorEl.textContent = 'Неверное значение. Введите число, например: 10000';
@@ -55,7 +45,14 @@ function handleSubmit(evt) {
     specialCashbackEl.textContent = `${result.specialCategoryCashback} руб.`;
     otherCashbackEl.textContent = `${result.otherCategoryCashback} руб.`;
     totalCashbackEl.textContent = `${result.totalCashback} руб.`;
-
 }
  const formEl = document.getElementById('cashback-form');
  formEl.onsubmit = handleSubmit;
+
+const specialAmountErrorEl = document.getElementById('special-amount-error');
+const otherAmountErrorEl = document.getElementById('other-amount-error');
+const specialCashbackEl = document.getElementById('special-cashback');
+const otherCashbackEl = document.getElementById('other-cashback');
+const totalCashbackEl = document.getElementById('total-cashback');
+const specialAmountInputEl = document.getElementById('special-amount-input');
+const otherAmountInputEl = document.getElementById('other-amount-input');
